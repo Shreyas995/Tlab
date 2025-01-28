a6c9851db81b8f147442c09024d5ef6eb0e2d966
jkostelecky <j.kostelecky@posteo.de>
2025-01-28 13:41:06 +0100

    debug

--- src/utils/linear3.f90
+++ src/utils/linear3.f90
@@ -548,10 +548,9 @@ call SYSTEM_CLOCK(clock_0,clock_cycle)
 
     wrk(1:len) = 0.0_wp
 
-    
-!    !$omp target teams distribute parallel do collapse(2) default(none) &
-!    !$omp private(n,l) &
-!    !$omp shared(wrk,nmax,d,f,len)
+    !$omp target teams distribute parallel do collapse(2) default(none) &
+    !$omp private(n,l) &
+    !$omp shared(wrk,nmax,d,f,len)
     do n = 1, nmax - 1
         ! dummy1 = d(n)
         do l = 1, len
@@ -559,7 +558,7 @@ call SYSTEM_CLOCK(clock_0,clock_cycle)
             ! wrk(l) = wrk(l) + dummy1*f(l, n)
         end do
     end do
-!    ! $omp end target teams distribute parallel do
+    !$omp end target teams distribute parallel do
 
     dummy1 = b(nmax)
     !$omp target teams distribute parallel do default(none) &
