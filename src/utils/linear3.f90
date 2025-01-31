a3fe04a9d3935146a8a40b9a9a884eee469d70ef
Shreyas <shreyasdeshpande16@gmail.com>
2025-01-31 16:37:14 +0100

    optimized offloading of TRIPDSS

--- src/utils/linear3.f90
+++ src/utils/linear3.f90
@@ -164,44 +164,24 @@ subroutine TRIDSS(nmax, len, a, b, c, f)
 #else
 
     ! Forward sweep
-    do n = 2, nmax
-        dummy1 = a(n)
-
-        !$omp target teams distribute parallel do default(none) &
-        !$omp private(l) &
-        !$omp shared(f,dummy1,n,len)
-        do l = 1, len
-            f(l, n) = f(l, n) + dummy1*f(l, n - 1)
-        end do
-        !$omp end target teams distribute parallel do
-
-    end do
-
-    ! Backward sweep
-    dummy1 = b(nmax)
-
     !$omp target teams distribute parallel do default(none) &
     !$omp private(l) &
-    !$omp shared(f,dummy1,nmax,len)
+    !$omp shared(f,dummy1,n,nmax,len,a,b,c)
     do l = 1, len
-        f(l, nmax) = f(l, nmax)*dummy1
+
+        do n = 2, nmax
+            f(l, n) = f(l, n) + a(n)*f(l, n - 1)
+        end do
+
+        ! Backward sweep
+
+        f(l, nmax) = f(l, nmax)*b(nmax)
+
+        do n = nmax - 1, 1, -1
+            f(l, n) = (f(l, n) + c(n)*f(l, n + 1))*b(n)
+        end do
     end do
     !$omp end target teams distribute parallel do
-
-    do n = nmax - 1, 1, -1
-        dummy1 = c(n)
-        dummy2 = b(n)
-
-        !$omp target teams distribute parallel do default(none) &
-        !$omp private(l) &
-        !$omp shared(f,dummy1, dummy2,n,len)
-        do l = 1, len
-            f(l, n) = (f(l, n) + dummy1*f(l, n + 1))*dummy2
-        end do
-        !$omp end target teams distribute parallel do
-
-
-    end do
     
 #endif
 
@@ -551,40 +531,6 @@ call SYSTEM_CLOCK(clock_0,clock_cycle)
         end do
     end do
     !$omp end target teams distribute parallel do
-
-    ! !$omp target teams distribute parallel do collapse (2) default(none) &
-    ! !$omp private(l,n) &
-    ! !$omp shared(f,len,nmax,wrk,a,b,c,d,e,idx,idx_p)
-    ! do l = 1, len
-    !     ! -------------------------------------------------------------------
-    !     ! Forward sweep
-    !     ! -------------------------------------------------------------------
-    !     f(l) = f(l) * b(1)
-
-    !     wrk(l) = 0.0_wp
-
-    !     do n = 2, nmax - 1
-    !         idx   = (n - 1) * len  ! Base index for column n
-    !         idx_p = (n - 2) * len  ! Base index for column n-1
-
-    !         f(idx + l) = f(idx + l) * b(n) + a(n) * f(idx_p + l)
-    !         wrk(l) = wrk(l) + d(n) * f(idx + l)
-    !     end do
-
-    !     wrk(l) = wrk(l) + d(1) * f(len + l)
-
-    !     f((nmax - 1) * len + l) = (f((nmax - 1) * len + l) - wrk(l)) * b(nmax)
-    !     ! -------------------------------------------------------------------
-    !     ! Backward sweep
-    !     ! -------------------------------------------------------------------
-    !     f((nmax - 2) * len + l) = e(nmax - 1) * f((nmax - 1) * len + l) + f((nmax - 2) * len + l)
-    !     do n = nmax - 2, 1, -1
-    !         f((n-1) * len + l) = f((n-1) * len + l) + c(n) * f(n * len + l) + e(n) * f((nmax - 1) * len + l)
-    !     end do
-    ! end do
-    ! !$omp end target teams distribute parallel do
-
-
 
 #endif
 
